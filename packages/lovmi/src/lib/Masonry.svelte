ffb03bded7282965f3da65c13e2cbecea3f863f2
rixo <rixo@rixo.fr>
2022-05-26 10:41:02 +0200

    throttle masonry

--- packages/lovmi/src/lib/Masonry.svelte
+++ packages/lovmi/src/lib/Masonry.svelte
@@ -1,9 +1,11 @@
 <script>
   import { onMount, onDestroy, getContext, setContext, tick } from "svelte"
+
   export let stretchFirst = false,
     gridGap = "0.5em",
     colWidth = "minmax(Min(20em, 100%), 1fr)",
     items = [] // pass in data if it's dynamically updated
+
   let grids = [],
     masonryElement
 
@@ -47,6 +49,32 @@
     })
   }
 
+  const throttle = (func, delay) => {
+    // Previously called time of the function
+    let prev = 0
+    return (...args) => {
+      // Current called time of the function
+      let now = new Date().getTime()
+
+      // Logging the difference between previously
+      // called and current called timings
+      console.log(now - prev, delay)
+
+      // If difference is greater than delay call
+      // the function again.
+      if (now - prev > delay) {
+        prev = now
+
+        // "..." is the spread operator here
+        // returning the function with the
+        // array of arguments
+        return func(...args)
+      }
+    }
+  }
+
+  const scheduleRefreshLayout = throttle(refreshLayout, 500)
+
   const calcGrid = async (_masonryArr) => {
     await tick()
     if (
@@ -71,13 +99,17 @@
   let _window
   onMount(() => {
     _window = window
-    _window.addEventListener("resize", refreshLayout, false) /* on resize */
+    _window.addEventListener(
+      "resize",
+      scheduleRefreshLayout,
+      false
+    ) /* on resize */
   })
   onDestroy(() => {
     if (_window) {
       _window.removeEventListener(
         "resize",
-        refreshLayout,
+        scheduleRefreshLayout,
         false
       ) /* on resize */
     }
