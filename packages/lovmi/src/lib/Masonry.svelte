87d959fc0a03fe56e10deca5845d4d16b13b7272
rixo <rixo@rixo.fr>
2022-05-27 23:40:20 +0200

    optimize refresh

--- packages/lovmi/src/lib/Masonry.svelte
+++ packages/lovmi/src/lib/Masonry.svelte
@@ -49,27 +49,25 @@
     })
   }
 
+  let finalRefreshTimeout
+
   const throttle = (func, delay) => {
     // Previously called time of the function
     let prev = 0
+    let now
+
+    const run = () => {
+      prev = now
+      refreshLayout()
+    }
+
     return (...args) => {
-      // Current called time of the function
-      let now = new Date().getTime()
-
-      // Logging the difference between previously
-      // called and current called timings
-      console.log(now - prev, delay)
-
-      // If difference is greater than delay call
-      // the function again.
+      now = new Date().getTime()
       if (now - prev > delay) {
-        prev = now
-
-        // "..." is the spread operator here
-        // returning the function with the
-        // array of arguments
-        return func(...args)
+        run()
       }
+      clearTimeout(finalRefreshTimeout)
+      finalRefreshTimeout = setTimeout(run, 200)
     }
   }
 
