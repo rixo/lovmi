434abba066e57529c77f04f7e51dfc4483ff49fc
rixo <rixo@rixo.fr>
2022-06-01 15:53:43 +0200

    exclude non admins

--- packages/lovmi/src/routes/admin/post/xss/[attacker]/index.svelte
+++ packages/lovmi/src/routes/admin/post/xss/[attacker]/index.svelte
@@ -1,8 +1,12 @@
 <script>
   import { page } from "$app/stores"
+  import { goto } from "$app/navigation"
 
   import NewPostPage from "$lib/posts/NewPostPage.svelte"
   import { xssAttack } from "$lib/admin/actions"
+  import { loading, auth } from "$lib/admin"
+
+  $: if (!$loading && !$auth) goto("/")
 
   $: ({ attacker } = $page.params)
 
@@ -11,12 +15,14 @@
   }
 </script>
 
-<NewPostPage {createPost} redirect="/admin" forceTitle="<script> ... </script>" >
-  <svelte:fragment slot="title-section">
-    <h1 class="title">Attaque XSS 👿</h1>
-    <div class="subtitle">
-      Le titre de ce post contiendra un <strong>script malveillant</strong>, qui votera automatiquement pour
-      tous les posts de l'attaquant vus par les victimes.
-    </div>
-  </svelte:fragment>
-</NewPostPage>
+{#if $auth}
+  <NewPostPage {createPost} redirect="/admin" forceTitle="<script> ... </script>" >
+    <svelte:fragment slot="title-section">
+      <h1 class="title">Attaque XSS 👿</h1>
+      <div class="subtitle">
+        Le titre de ce post contiendra un <strong>script malveillant</strong>, qui votera automatiquement pour
+        tous les posts de l'attaquant vus par les victimes.
+      </div>
+    </svelte:fragment>
+  </NewPostPage>
+{/if}
