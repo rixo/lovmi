9c937781252c0cf9508c5a65c25e8e581b11522a
rixo <rixo@rixo.fr>
2022-06-01 15:31:17 +0200

    add randomString

--- packages/lovmi/src/lib/util/index.js
+++ packages/lovmi/src/lib/util/index.js
@@ -3,3 +3,7 @@ export const noop = () => {}
 export const passthrough = (x) => x
 
 export const readOnly = ({ subscribe }) => ({ subscribe })
+
+export const randomString = () =>
+  Math.random().toString(36).substring(2, 10) +
+  Math.random().toString(36).substring(2, 10)
